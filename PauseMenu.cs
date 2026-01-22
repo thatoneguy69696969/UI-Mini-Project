2d5324a8ba84d5cdaa00edaf048f9a4bec6c1915
Tristen <tthompson3@ftstudent.org>
2026-01-22 12:19:33 -0500

    Fixed pause
    just cuz LMAO

--- PauseMenu.cs
+++ PauseMenu.cs
@@ -5,30 +5,20 @@ public partial class PauseMenu : Control
 {
 	public override void _Process(double delta)
 	{
-		TestEsc();
+		if (Input.IsActionJustPressed("escape"))
+		{
+			GetTree().Paused=!GetTree().Paused;
+		}
 	}
 
 	public void Resume()
 	{
 		GetTree().Paused = false;
+		
 	}
 
 	public void Pause()
 	{
 		GetTree().Paused = true;
-	}
-
-	private void TestEsc()
-	{
-		if (Input.IsActionJustPressed("escape"))
-		{
-			{
-			GD.Print("User Pressed Escape");
-			if (GetTree().Paused)
-				Resume();
-			else
-				Pause();
-			}
-		}
 	}
 }
